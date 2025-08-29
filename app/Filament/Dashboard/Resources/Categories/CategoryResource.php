0a37b34fb779e0c36dfebc2330b6bcde686356e5
Lucas Tonolli <lucastonolli973@gmail.com>
2025-08-29 16:29:13 -0300

    add Dashboard Categories Page

---
+++ app/Filament/Dashboard/Resources/Categories/CategoryResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\Categories;
+
+use App\Filament\Dashboard\Resources\Categories\Pages\ManageCategories;
+use App\Models\Category;
+use BackedEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $modelLabel = 'Categoria';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(50)
+                    ->label('Nome'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->label('Nome'),
+                TextColumn::make('slug')
+                    ->searchable(),
+                TextColumn::make('posts.count')
+                    ->sortable()
+                    ->label('Posts')
+
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageCategories::route('/'),
+        ];
+    }
+}
